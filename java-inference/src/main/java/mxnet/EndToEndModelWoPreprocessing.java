23e0ef4132c1c3ce6e5afaf7b7130d862fa20b19
stu1130 <gstu1130@gmail.com>
2019-02-10 21:47:13 -0800

    update the java code

--- java-inference/src/main/java/mxnet/EndToEndModelWoPreprocessing.java
+++ java-inference/src/main/java/mxnet/EndToEndModelWoPreprocessing.java
@@ -62,7 +62,7 @@ public class EndToEndModelWoPreprocessing {
         NDArray resizeImg;
         if (isBatch) {
             NDArray[] arr = new NDArray[nd.shape().get(0)];
-            for (int i = 0; i < nd.shape().get(0)) {
+            for (int i = 0; i < nd.shape().get(0); i++) {
                 arr[i] = Image.imResize(nd.at(i), 224, 224));
             }
             resizeImg = NDArray.stack(arr, 0, arr.length, null)[0];
@@ -143,7 +143,7 @@ public class EndToEndModelWoPreprocessing {
 
         String imgPath = inst.inputImagePath;
         String imgDir = inst.inputImageDir;
-        boolean isBatch = Boolean.parseBoolean(inst.batchFlag;
+        boolean isBatch = Boolean.parseBoolean(inst.batchFlag);
 
         // Prepare the model
         List<Context> context = new ArrayList<Context>();
