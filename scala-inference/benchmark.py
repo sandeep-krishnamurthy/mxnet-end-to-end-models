81475d147d9378c0267ec5d578ad43fad3f77511
stu1130 <gstu1130@gmail.com>
2019-02-12 17:18:40 -0800

    update the script

--- scala-inference/benchmark.py
+++ scala-inference/benchmark.py
@@ -5,11 +5,12 @@ import sys
 
 sum_e2e = 0.0
 sum_non_e2e = 0.0
-for i in range(10):
+num_iter = 1
+for i in range(num_iter):
     output = subprocess.check_output('bash bin/run_im.sh',
     stderr=subprocess.STDOUT,
     shell=True).decode(sys.stdout.encoding)
     res = re.search('E2E\nsingle_inference_average (\d+.\d+)ms\nNon E2E\nsingle_inference_average (\d+.\d+)ms', output)
     sum_e2e += float(res.group(1))
     sum_non_e2e += float(res.group(2))
-print('E2E {} Non E2E {}'.format(sum_e2e / 10.0, sum_non_e2e / 10.0))
+print('E2E {} Non E2E {}'.format(sum_e2e / num_iter, sum_non_e2e / num_iter))
