8a391b009a22b949219436843b550ce389f3e341
stu1130 <gstu1130@gmail.com>
2019-02-15 11:59:35 -0800

    finish the shell script

--- scala-inference/bin/run_benchmark.sh
+++ scala-inference/bin/run_benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 if [[ $2 = gpu ]]
 then
@@ -36,19 +36,26 @@ output_single=$(java -Xmx8G  -cp $CLASSPATH mxnet.EndToEndModelWoPreprocessing \
 --warm-up 5 \
 $end_to_end \
 $use_gpu)
-# sum=0.0
-# # the defualt value is 20 so tha we have enough CPU and GPU memory
-# num_iter=1
-# for n in `seq 1 $num_iter`
-# do
-#     output_batch=$(java -Xmx8G  -cp $CLASSPATH mxnet.EndToEndModelWoPreprocessing \
-#     --model-path-prefix $model_path \
-#     --num-runs 20 \
-#     --batchsize 25 \
-#     --warm-up 5 \
-#     $end_to_end \
-#     $use_gpu)
-#     echo $output_batch | grep ""
-# done
-echo $output_single
-# echo $output_batch
+
+sum=0.0
+count=0
+# the defualt value is 20 so tha we have enough CPU and GPU memory
+num_iter=$(echo $3/25 | bc)
+if [ $num_iter = 0 ]; then num_iter=1; fi
+for n in `seq 1 $num_iter`
+do
+    output_batch=$(java -Xmx8G  -cp $CLASSPATH mxnet.EndToEndModelWoPreprocessing \
+    --model-path-prefix $model_path \
+    --num-runs 25 \
+    --batchsize 25 \
+    --warm-up 1 \
+    $end_to_end \
+    $use_gpu)
+    value=$(echo $output_batch | ggrep -oP '(E2E|Non E2E) (single|batch)_inference_average \K(\d+.\d+)(?=ms)')
+    sum=$(echo $sum+$value | bc)
+    ((count++))
+    echo $value
+done
+
+metrix=$(echo $output_batch | ggrep -oE '(single|batch)_inference_average')
+echo "$output_single $metrix $(echo "scale=2; $sum/$count" | bc)ms"
