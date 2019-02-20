2067112d7ce46a490082d019c51abf874d703afa
stu1130 <gstu1130@gmail.com>
2019-02-20 10:01:16 -0800

    update the input image

--- blog-post/bin/get_model.sh
+++ blog-post/bin/get_model.sh
@@ -5,4 +5,4 @@ set -ex
 wget https://s3.us-east-2.amazonaws.com/mxnet-public/end_to_end_models/resnet18_v1_end_to_end-symbol.json
 wget https://s3.us-east-2.amazonaws.com/mxnet-public/end_to_end_models/resnet18_v1_end_to_end-0000.params
 wget https://s3.us-east-2.amazonaws.com/scala-infer-models/resnet-18/synset.txt
-wget https://s3.us-east-2.amazonaws.com/mxnet-scala/scala-example-ci/resnet152/kitten.jpg
+wget https://s3.amazonaws.com/model-server/inputs/Pug-Cookie.jpg
